6d1f7f96852effbf1f0abbf083e2b6285c57ab1d
Gustavo Moura <gsoaaresmoura@gmail.com>
2025-03-12 00:06:50 -0300

    added angular material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ComponentComponent } from './component/component.component';
+import { TestComponent } from './test/test.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ComponentComponent,
+    TestComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/component/component.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-component',
+  standalone: false,
+  templateUrl: './component.component.html',
+  styleUrl: './component.component.css'
+})
+export class ComponentComponent {
+
+}
