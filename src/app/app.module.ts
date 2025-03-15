501e71145cb87a01cf77da5a769364f3f39ab140
Gustavo Moura <gsoaaresmoura@gmail.com>
2025-03-15 01:27:22 -0300

    added home and not found routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,18 +7,24 @@ import { HeaderComponent } from './header/header.component';
 import { FormsModule } from '@angular/forms';
 import { MaterialModule } from './material.module';
 import { FooterComponent } from './footer/footer.component';
+import { HomeComponent } from './home/home.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { CategoryComponent } from './category/category.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     FooterComponent,
+    HomeComponent,
+    PageNotFoundComponent,
+    CategoryComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     FormsModule,
-    MaterialModule
+    MaterialModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/page-not-found/page-not-found.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-page-not-found',
+  standalone: false,
+  templateUrl: './page-not-found.component.html',
+  styleUrl: './page-not-found.component.css'
+})
+export class PageNotFoundComponent {
+
+  constructor() { }
+
+  ngOnInit(): void {
+    
+  }
+
+}
